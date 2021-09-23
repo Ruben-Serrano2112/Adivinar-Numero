83ac3fcf2919292fbf1c79ecc68de2c334a39bbc
Ruben <ruben.serrano@iesdoctorbalmis.com>
2021-09-23 15:02:52 +0200

    Agregar archivos de proyecto.

---
+++ Adivinar Numero/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Windows;
+
+
+namespace Adivinar_Numero
+{
+    public partial class MainWindow : Window
+    {
+        Random numero = new Random();
+        int num;
+
+        public MainWindow()
+        { 
+            InitializeComponent();
+            num = numero.Next(0, 100);
+        }
+
+        private void Reiniciar_button(object sender, RoutedEventArgs e)
+        {
+            num = numero.Next(0, 100);
+        }
+
+        private void Comprobar_button(object sender, RoutedEventArgs e)
+        {
+            int numAComp = int.Parse(NombreTextBox.Text);
+            if(numAComp > num)
+            {
+                Comprobacion.Text="Te has alejado";
+            }
+            else if(numAComp < num)
+            {
+                Comprobacion.Text = "Te has quedado corto";
+            }
+            else
+            {
+                Comprobacion.Text = "Has acertado";
+            }
+        }
+    }
+}
